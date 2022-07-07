bad6f0e0999008a2b0973f09ba499f06ed0cec49
Es-Kay-Darling <eskayblay@gmail.com>
2022-07-07 01:23:23 +0000

    advanced

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-      """
-        Parameters
-        ----------
-        filename: dictionary with key and value
-        Returns
-        ----------
-        Lists
-        """
     a_dictionary[key] = value
-    return a_dictionary
+    return (a_dictionary)
